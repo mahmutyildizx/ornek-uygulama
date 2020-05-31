5ae172f928fa08a55bb0ad224b29b4a740b2841f
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2020-05-31 15:33:25 +0300

    Updates

---
+++ precache-manifest.474f42d1d5f338b2105c76f3275423ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb703741dac4b36ae3ba0adc6a13315",
+    "url": "/ornek-uygulama/index.html"
+  },
+  {
+    "revision": "e19788a29c6afa21c5fa",
+    "url": "/ornek-uygulama/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e4ffc536249b0d13619",
+    "url": "/ornek-uygulama/static/js/2.c058ff14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ornek-uygulama/static/js/2.c058ff14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19788a29c6afa21c5fa",
+    "url": "/ornek-uygulama/static/js/main.2b4c68e4.chunk.js"
+  },
+  {
+    "revision": "3e2e4e8f83922c542912",
+    "url": "/ornek-uygulama/static/js/runtime-main.674c2255.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ornek-uygulama/static/media/logo.5d5d9eef.svg"
+  }
+]);
